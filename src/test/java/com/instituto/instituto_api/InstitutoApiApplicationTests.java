3588f832d5b024c131797000b5d2388c5d72e249
Jose Ariel Apaza <lpze.joseariel.apaza.ch@unifranz.edu.bo>
2026-05-08 15:17:41 -0400

    feat: agregar wrapper de maven y tests base

---
+++ src/test/java/com/instituto/instituto_api/InstitutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instituto.instituto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
