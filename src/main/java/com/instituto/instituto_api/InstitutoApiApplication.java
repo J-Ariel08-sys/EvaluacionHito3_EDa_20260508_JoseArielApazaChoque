5ac7b17a647269605316968048b129eeba077064
Jose Ariel Apaza <lpze.joseariel.apaza.ch@unifranz.edu.bo>
2026-05-08 14:40:17 -0400

    feat: inicializar proyecto Spring Boot para sistema de instituto
    Estructura base generada con Spring Initializr.

---
+++ src/main/java/com/instituto/instituto_api/InstitutoApiApplication.java
@@ -0,0 +1,13 @@
+package com.instituto.instituto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoApiApplication.class, args);
+	}
+
+}
